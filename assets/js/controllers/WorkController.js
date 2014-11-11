4a9fb215c8f1bcc38ffdcbde238d66405e382b28
Tanotify <toropanov@gmail.com>
2014-11-11 15:36:24 +0400

    Started to add class Selected, cannot solve animation

--- assets/js/controllers/WorkController.js
+++ assets/js/controllers/WorkController.js
@@ -1,8 +1,28 @@
-app.controller('LatestWorkController', function($scope, $http) {
+app.controller('LatestWorkController', function($scope, $http, $element) {
     $http.get('data.json')
         .then(function(res){
             $scope.works = res.data.works;
         });
+
+    $scope.showWork= function(item) {
+        $scope.selected = item;
+    };
+
+    $scope.isActive = function(item) {
+        return $scope.selected === item;
+    };
+});
+
+app.animation('.eachWork', function() {
+    return {
+        addClass : function(element, className, done) {
+            element.addClass('.selected');
+            element.prependTo($(".listWork"));
+            console.log("fds");
+        },
+        removeClass : function(element, className, done) {
+        }
+    }
 });
 
 app.controller('WorkController', function($scope, $routeParams) {
