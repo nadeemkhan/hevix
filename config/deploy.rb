4e696fcd3df453551ad41e7b5d7b2510875e0fcc
Tanotify <tanotify@gmail.com>
2015-09-01 23:34:36 +0300

    Changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '104.131.74.97', port: your_port_num, roles: [:web, :app, :db], primary: true
+server '104.131.74.97', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@bitbucket.org:tanotify/toropanov.com.git'
 set :application,     'Toropanov.com'
