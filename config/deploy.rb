3582ada207586f3aa1c018cdac84474cc95eb1ee
tanotify <toropanov@gmail.com>
2015-09-21 22:00:43 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -84,14 +84,42 @@ namespace :deploy do
   end
 
   before :starting,     :check_revision
+  after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
 
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
+
+# Below is capsistrano Mysql Sync
+
 # if you haven't already specified
 set :rails_env, "production"
+
+# if you want to remove the local dump file after loading
+set :db_local_clean, true
+
+# if you want to remove the dump file from the server after downloading
+set :db_remote_clean, true
+
+# if you want to exclude table from dump
+set :db_ignore_tables, []
+
+# if you want to exclude table data (but not table schema) from dump
+set :db_ignore_data_tables, []
 
 # If you want to import assets, you can change default asset dir (default = system)
 # This directory must be in your shared directory on the server
 set :assets_dir, %w(public/assets public/att)
 set :local_assets_dir, %w(public/assets public/att)
+
+# if you want to work on a specific local environment (default = ENV['RAILS_ENV'] || 'development')
+set :locals_rails_env, "production"
+
+# if you are highly paranoid and want to prevent any push operation to the server
+set :disallow_pushing, false
+
+# if you prefer bzip2/unbzip2 instead of gzip
+set :compressor, :bzip2
